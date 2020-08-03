6484ca554e4d1eae9d050f0a2516527bd2564891
screen24 <zhafari2405@gmail.com>
2020-08-03 23:42:28 +0700

    Pekan 3 Tugas Hari Ke-1 Sanbercode

---
+++ src/tugas11/tugas11.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './tugas11.css'
+
+
+let dataHargaBuah = [
+    {nama: "Semangka", harga: 10000, berat: 1000},
+    {nama: "Anggur", harga: 40000, berat: 500},
+    {nama: "Strawberry", harga: 30000, berat: 400},
+    {nama: "Jeruk", harga: 30000, berat: 1000},
+    {nama: "Mangga", harga: 30000, berat: 500}
+]
+class HargaBuah extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Tabel Harga Buah</h1>
+                <table>
+                    <tr>
+                        <th>Nama</th>
+                        <th>Harga</th>
+                        <th>Berat</th>
+                    </tr>
+                    <>
+                        {dataHargaBuah.map(dat => {
+                            return (
+                                <tr>
+                                    <td>{dat.nama}</td>
+                                    <td>{dat.harga}</td>
+                                    <td>{dat.berat}</td>
+                                </tr>
+                            )
+                        })}
+                    </>
+                </table>
+            </div>
+        )        
+    }
+}
+
+
+export default HargaBuah
